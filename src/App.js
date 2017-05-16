de6dc93b4f2ba9922b1bb34bbf3c97a1436f2d27
Angela <Baby007mix@gmail.com>
2017-05-16 18:55:45 +0100

    changes to styling and card

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Organisation from './pages/Organisation';
 import SidebarElement from './pages/SidebarElement';
 import logoSidebar from '../public/assets/logo-sidebar.svg';
 import APIs from './APIs';
-import OrganisationCard from './pages/OrganisationCard';
+import Card from './pages/Card';
 
 const weekDays = [
   'Monday',
@@ -147,7 +147,7 @@ class App extends Component {
             <h3>{this.state.title}</h3>
             {
               this.state.organisationList.map(organisation =>
-                <OrganisationCard
+                <Card
                   {...organisation}
                   key={organisation[1]}
                 />)
